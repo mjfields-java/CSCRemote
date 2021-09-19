ccf2d2f962f096dcd2a73160c5da20e8af00bc76
Michael Fields <michaelfieldsorganization@gmail.com>
2021-09-19 13:18:39 -0400

    first commit

--- gitremote/test/unit/test_whats_my_name.py
+++ gitremote/test/unit/test_whats_my_name.py
@@ -4,4 +4,5 @@ from gitremote.whats_my_name import my_name_is
 
 
 def test_my_name_is():
-    assert "Joseph" == my_name_is()
+    # noinspection PyInterpreter
+    assert "mjfields" == my_name_is()
